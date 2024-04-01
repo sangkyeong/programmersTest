daf545a78319221a18a9f367a6f5ff2ed6fa6087
rlatkdrud123@naver.com <rlatkdrud123@naver.com>
2024-04-01 21:57:07 +0900

    240401 프로그래머스 Lv3_이중우선순위큐(해결완료)

---
+++ test/src/programmersTest/test240401_2110_LV3_eZungPriority.java
@@ -0,0 +1,47 @@
+package programmersTest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class test240401_2110_LV3_eZungPriority {
+    public static int[] solution(String[] operations) {
+		int[] answer = {0,0};
+		ArrayList<Integer> list = new ArrayList<>();
+
+        for (String operation : operations) {
+            String[] mem = operation.split(" ");
+            for (int j = 0; j < mem.length; j++) {
+                switch (mem[j]) {
+                    case "I":
+                        list.add(Integer.parseInt(mem[j + 1]));
+                        Collections.sort(list);
+                        break;
+                    case "D":
+						if ((!list.isEmpty()) && (mem[j + 1].equals("1"))) {
+							list.removeLast();
+						} else if ((!list.isEmpty()) && (mem[j + 1].equals("-1"))) {
+                            list.removeFirst();
+                        }
+                        break;
+                }
+
+            }
+        }
+
+		if(!list.isEmpty()){
+			answer[0] = list.getLast();
+			answer[1] = list.getFirst();
+		}
+		System.out.println(Arrays.toString(answer));
+		return answer;
+	}
+
+	public static void main(String[] args){
+		String[] new_id = {"I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"};
+		
+		System.out.println(solution(new_id));
+		//S0ystem.out.println(Arrays.toString(solution(N)));
+	}
+}
