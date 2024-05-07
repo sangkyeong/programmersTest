b7facfae94f737cab05e4ebaf969c52e73651ef4
rlatkdrud123@naver.com <rlatkdrud123@naver.com>
2024-05-07 23:07:34 +0900

    240507 프로그래머스 Lv1_약수의 개수와 덧셈

--- test/src/programmersTest/test240507_2148_LV1_divCntSum.java
+++ test/src/programmersTest/test240507_2148_LV1_divCntSum.java
@@ -4,15 +4,13 @@ public class test240507_2148_LV1_divCntSum {
     public static int solution(int left, int right) {
 		int answer = 0;
 		for(int i=left;i<=right;i++){
-			int num = (int)Math.sqrt(i);
 			int cnt = 0;
-			for(int j=1;j<=num;j++){
-				if(num%j == 0){
+			for(int j=1;j<=i;j++){
+				if(i%j == 0){
 					cnt++;
-					System.out.println(j);
 				}
 			}
-			if((cnt*2)%2 == 0)answer += i;
+			if(cnt%2 == 0)answer += i;
 			else answer -= i;
 		}
 		return answer;
